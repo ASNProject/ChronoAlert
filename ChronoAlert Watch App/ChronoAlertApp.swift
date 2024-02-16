e880ac06a6d5b4d0df1a12f68e13bbb9df6f6cff
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2024-02-16 09:34:43 +0700

    Initial Commit

---
+++ ChronoAlert Watch App/ChronoAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoAlertApp.swift
+//  ChronoAlert Watch App
+//
+//  Created by Arief Setyo Nugroho on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ChronoAlert_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
